737674f8b4fa13a3f454b77ca993d4fd807c9c1d
mifaabiyyu <mifaabiyyu@gmail.com>
2023-07-16 22:37:35 +0700

    fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
 const Footer: React.FC = () => {
   return (
     <footer>
-      <div className='fixed bg-white bottom-0 left-0 flex h-16 w-full items-end justify-center  from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
+      <div className='fixed bg-white bottom-0 left-0 flex h-14 w-full items-end justify-center  from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none'>
         <a
-          className=' flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0'
+          className=' flex place-items-center gap-2 p-4 lg:pointer-events-auto lg:p-0'
           href='https://mifa-abiyyu.vercel.app'
           target='_blank'
           rel='noopener noreferrer'>
